263c4e922baa0b67698b9e76fb294a784b5b6d71
AriyovaBanua <ariyovabanua@gmail.com>
2023-06-13 02:46:45 +0700

    ubah tampilan login

--- app/Views/login/loginpage.php
+++ app/Views/login/loginpage.php
@@ -4,17 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <title>Login Form</title>
 </head>
 
 <body>
-    <form action="/login/check" method="post">
-        <?= csrf_field() ?>
-        User: <input type="text" name="username" /><br>
-        Password: <input type="text" name="password" /><br>
-        <input type="submit" name="submit" value="Login" /><br>
-    </form>
+    <div class="container">
+        <h1 class="mt-4">Login Form</h1>
+        <form action="/login/check" method="post">
+            <?= csrf_field() ?>
+            <div class="mb-3">
+                <label for="username" class="form-label">User:</label>
+                <input type="text" class="form-control" id="username" name="username">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password:</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
