2e3a514383419c3f577c4798aec3dc0392b2a822
AriyovaBanua <ariyovabanua@gmail.com>
2023-06-13 02:35:00 +0700

    ubah tampilan delete

--- app/Views/asisten/delete.php
+++ app/Views/asisten/delete.php
@@ -5,16 +5,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <title>Form Pendaftaran</title>
 </head>
 
 <body>
-    <h1>DELETE DATA ASISTEN</h1>
-    <form action="/asisten/delete" method="post">
-        <?= csrf_field() ?>
-        NIM:<input type="text" name="nim" /><br />
-        <input type="submit" name="" value="Delete" />
-    </form>
+    <div class="container">
+        <h1 class="mt-4">DELETE DATA ASISTEN</h1>
+        <form action="/asisten/delete" method="post">
+            <?= csrf_field() ?>
+            <div class="mb-3">
+                <label for="nim" class="form-label">NIM:</label>
+                <input type="text" class="form-control" id="nim" name="nim">
+            </div>
+            <button type="submit" class="btn btn-danger">Delete</button>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
