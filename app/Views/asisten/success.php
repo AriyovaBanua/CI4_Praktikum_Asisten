ff3afb52d9c981724f33cecd5f558cd8e84c608e
AriyovaBanua <ariyovabanua@gmail.com>
2023-06-13 02:30:16 +0700

    ubah tampilan success

--- app/Views/asisten/success.php
+++ app/Views/asisten/success.php
@@ -5,12 +5,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <title>Status</title>
 </head>
 
 <body>
-    <h1>Data sukses tersimpan</h1>
-    <a href="/asisten">Lihat Data Asisten</a>
+    <div class="container">
+        <h1 class="mt-4">Data sukses tersimpan</h1>
+        <a href="/asisten" class="btn btn-primary mt-3">Lihat Data Asisten</a>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
