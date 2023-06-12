7899d163589aef09c07de95aba1770b538badc43
AriyovaBanua <ariyovabanua@gmail.com>
2023-06-13 02:18:47 +0700

    tambah bootstrap di halaman simpan

--- app/Views/asisten/simpan.php
+++ app/Views/asisten/simpan.php
@@ -6,18 +6,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Pendaftaran</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
-    <h1>PENDAFTARAN ASISTEN PRAKTIKUM</h1>
-    <form action="/asisten/simpan" method="post">
-        <?= csrf_field() ?>
-        NIM:<input type="text" name="nim" /><br />
-        NAMA:<input type="text" name="nama" /><br />
-        PRAKTIKUM:<input type="text" name="praktikum" /><br />
-        IPK:<input type="text" name="ipk" /><br />
-        <input type="submit" name="" value="Simpan" />
-    </form>
+    <div class="container">
+        <h1 class="mt-4">PENDAFTARAN ASISTEN PRAKTIKUM</h1>
+        <form action="/asisten/simpan" method="post">
+            <?= csrf_field() ?>
+            <div class="mb-3">
+                <label for="nim" class="form-label">NIM:</label>
+                <input type="text" class="form-control" id="nim" name="nim">
+            </div>
+            <div class="mb-3">
+                <label for="nama" class="form-label">NAMA:</label>
+                <input type="text" class="form-control" id="nama" name="nama">
+            </div>
+            <div class="mb-3">
+                <label for="praktikum" class="form-label">PRAKTIKUM:</label>
+                <input type="text" class="form-control" id="praktikum" name="praktikum">
+            </div>
+            <div class="mb-3">
+                <label for="ipk" class="form-label">IPK:</label>
+                <input type="text" class="form-control" id="ipk" name="ipk">
+            </div>
+            <button type="submit" class="btn btn-primary">Simpan</button>
+        </form>
+    </div>
 </body>
 
 </html>
